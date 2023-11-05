859dd4335f28782f1c144b9c727962e4ddef3de5
Jasraj Chouhan <jasrajc945@gmail.com>
2023-11-05 17:05:30 +0530

    connect to mongo db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,21 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGO)
+        .then(() => {
+            console.log("Connected to MongoDB");
+        })
+
+        .catch((err) => {
+            console.log(err);
+        })
 
 const app = express();
 
 app.listen(5173, () => {
     console.log('Server listening on port 5173!');
-});
+} );
